c1edee28c36b27efc76a892a4a583e282e8e0631
tl32rodan <tl32rodan.cs09g@nctu.edu.tw>
2020-08-05 16:55:39 +0800

    Modify dataloader to not resize the images...

--- dataloader.py
+++ dataloader.py
@@ -37,8 +37,6 @@ class RetinopathyLoader(data.Dataset):
         #           Transpose the image shape from [H, W, C] to [C, H, W]
         # We can just use transform.ToTensor() to acheive it
         self.trans = transforms.Compose([
-            transforms.Resize((256, 256)),
-            transforms.RandomCrop((224, 224)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                std=[0.229, 0.224, 0.225])
